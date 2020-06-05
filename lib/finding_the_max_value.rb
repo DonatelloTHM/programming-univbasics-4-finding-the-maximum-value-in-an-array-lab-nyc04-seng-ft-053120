3a43ed0d35d5e2fb2ce7a4db3081fd1fcf7ca6d8
Donat Pllana <donat.pllana@outlook.com>
2020-06-05 22:26:16 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,11 @@
 def find_max_value(array)
   # Add your solution here
+  max=nil
+  count=0
+  while count<array.length do
+    if(array[count]>max)
+      max=array[count]
+    end
+  end
+  max
 end
