4df67984d4cc55c3300162452049611e60e4b727
Donat Pllana <donat.pllana@outlook.com>
2020-06-05 22:31:41 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,10 +1,11 @@
 def find_max_value(array)
-  max=0
+  max_element=0
   count=0
   while count<array.length do
-    if(array[count]>max)
-      max=array[count]
+    if array[count] > max_element
+      max_element=array[count]
     end
     count+=1
   end
+  max_element
 end
